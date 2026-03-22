5dd9b12f20a920e66cebdd7ac9087ca316ad16c6
jebasinghsundersoni <jebasinghsundersoni@gmail.com>
2026-03-22 19:59:58 +0530

    Model reverted and endpoint working correctly

--- GroundWater_Project/app.py
+++ GroundWater_Project/app.py
@@ -6,6 +6,9 @@ app = Flask(__name__)
 
 model = pickle.load(open("model.pkl", "rb"))
 
+@app.route("/")
+def home():
+    return "Welcome to the Groundwater Level Prediction API!"
 @app.route("/predict", methods=["POST"])
 def predict():
     data = request.get_json()
@@ -15,8 +18,10 @@ def predict():
     residents = data["residents"]
     daily_usage = data["daily_usage"]
     rainfall = data["rainfall"]
-    
-    input_data = np.array([[tank_capacity, current_level, residents, daily_usage, rainfall]])
+    tank_fill_ratio = current_level/tank_capacity
+    usage_per_person = daily_usage /residents
+
+    input_data = np.array([[tank_capacity, current_level, residents, daily_usage, rainfall,tank_fill_ratio, usage_per_person]])
     
     prediction = model.predict(input_data)[0]
     
@@ -32,3 +37,8 @@ def predict():
         "days_left": round(prediction, 2),
         "alert": alert
     })
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+    
